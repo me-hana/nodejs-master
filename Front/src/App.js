10e302adcc63dd079dce53bb5eefa2308a3e0997
me-hana <hayonleee@gmail.com>
2022-02-28 19:36:33 +0900

    [FEAT] add cookie methods

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,30 +1,72 @@
-import React, { useState } from "react";
-import { styled } from "@mui/material/styles";
-import axios from "axios";
-import Cookies from "universal-cookie";
-import { Button, Input } from "@mui/material";
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+import { Button, Input } from '@mui/material';
 
-const ContentWrapper = styled("div")(() => ({
-  margin: "100px",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  ".button-area": {
-    marginTop: "30px",
+const ContentWrapper = styled('div')(() => ({
+  margin: '100px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  '.button-area': {
+    marginTop: '30px',
   },
 }));
 
 const StyledButton = styled(Button)(() => ({
-  margin: "10px",
+  margin: '10px',
 }));
 
 function App() {
-  const [info, setInfo] = useState({ id: "", pw: "" });
+  const [info, setInfo] = useState({ id: '', pw: '' });
+  const cookies = new Cookies();
+
+  const setTestCookie = () => {
+    cookies.set(
+      'test-cookie',
+      { name: 'chanchan', nickname: 'boba' },
+      {
+        path: '/',
+        // expires: Math.floor(Date.now() / 1000) + (60 * 60)
+      }
+    );
+  };
+
+  const loginApi = async (user) => {
+    axios
+      .post('api/user/login', user, { withCredentials: true })
+      .then((res) => {
+        if (res.data.state) {
+          console.log('로그인 요청', res);
+        } else {
+          console.log('요청은 성공했으나 서버 에러', res);
+        }
+      })
+      .catch((err) => {
+        console.log('로그인 요청 에러', err);
+      });
+  };
+
+  const logoutApi = async () => {
+    axios
+      .post('api/user/logout', { withCredentials: true })
+      .then((res) => {
+        if (res.data.state) {
+          console.log('로그아웃 요청', res);
+        } else {
+          console.log('요청은 성공했으나 서버 에러', res);
+        }
+      })
+      .catch((err) => {
+        console.log('로그아웃 요청 에러', err);
+      });
+  };
 
   return (
     <ContentWrapper>
-      <div style={{ fontSize: "3rem", margin: "50px" }}>쿠키 테스트</div>
+      <div style={{ fontSize: '3rem', margin: '50px' }}>쿠키 테스트</div>
       <Input
         placeholder="ID"
         onChange={(e) => {
@@ -38,9 +80,38 @@ function App() {
         }}
       />
       <div className="button-area">
-        <StyledButton variant="contained">로그인</StyledButton>
-        <StyledButton variant="contained">로그아웃</StyledButton>
-        <StyledButton variant="contained">universal</StyledButton>
+        <StyledButton
+          variant="contained"
+          onClick={() => {
+            loginApi(info);
+          }}
+        >
+          로그인
+        </StyledButton>
+        <StyledButton
+          variant="contained"
+          onClick={() => {
+            logoutApi();
+          }}
+        >
+          로그아웃
+        </StyledButton>
+        <StyledButton
+          variant="contained"
+          onClick={() => {
+            setTestCookie();
+          }}
+        >
+          universal 쿠키 만들기
+        </StyledButton>
+        <StyledButton
+          variant="contained"
+          onClick={() => {
+            console.log('쿠키 볼래!', cookies.get('test-cookie'));
+          }}
+        >
+          universal 쿠키 보기
+        </StyledButton>
       </div>
     </ContentWrapper>
   );
--- index.js
+++ index.js
@@ -1,9 +1,63 @@
-const express = require("express");
-const app = express();
-const port = 7777;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+const port = 7777;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(
+  session({
+    key: 'user_info',
+    secret: 'cookie-test',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60, // 1분
+    },
+  })
+);
+// app.use(express.static("public"));
+
+app.post('/api/user/login', (req, res, next) => {
+  console.log('리퀘스트 바디', req.body);
+  req.session.user_info = req.body;
+  req.session.save((err) => {
+    if (err) console.log('세션 저장 실패', err);
+  });
+  res.json({ state: true, message: 'session test' });
+});
+
+app.post('/api/user/logout', (req, res) => {
+  if (req.session.user_info) {
+    req.session.destroy((err) => {
+      if (err) {
+        console.log('세션 삭제 실패', err);
+        res.json({
+          state: false,
+          message: 'logout fail',
+        });
+      } else {
+        res.clearCookie('user_info');
+        res.json({
+          state: true,
+          message: 'logout success',
+        });
+      }
+    });
+  } else {
+    res.json({ state: true, message: 'logout success' });
+  }
 });
 
 app.listen(port, () => {
